dc8e6437ba06b8b7a8282c4739b356d0583f5acd
赵雅国 <zhao.yaguo@shinewingdigital.com>
2021-12-09 16:45:31 +0800

    feat(rust struct)

--- src/demo/mod.rs
+++ src/demo/mod.rs
@@ -1,7 +1,9 @@
 pub mod demo1;
 pub mod demo2;
 pub mod demo3;
+pub mod demo4;
 
 pub use demo1::*;
 pub use demo2::*;
 pub use demo3::*;
+pub use demo4::*;
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 mod demo;
+use crate::demo::City;
+use crate::demo::Color;
 
 fn main() {
     
     // demo 1 print and pr
-    demo::print_function();
+    // demo::print_function();
 
     // demo 2 debug 什么。。。看不懂
-    demo::fn_debug();
+    // demo::fn_debug();
 
     // demo 3 基础语法
-    demo::variable_fn();
+    // demo::variable_fn();
+
+    // demo4 formatting
+    // demo::formatting_fn();
+
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{:?}", *color);
+    }
+
+
 
     let stdout = stdout();
     let message = String::from("Hello Fellow Rustaceans");
