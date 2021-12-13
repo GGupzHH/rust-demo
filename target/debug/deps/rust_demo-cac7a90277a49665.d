6f6f2562881c8b7b6cb60964fc92d4c7deaa08df
赵雅国 <zhao.yaguo@shinewingdigital.com>
2021-12-13 16:50:19 +0800

    fix(error)

---
+++ target/debug/deps/rust_demo-cac7a90277a49665.d
@@ -0,0 +1,5 @@
+/Users/mzc/Documents/project_self/rust-demo/target/debug/deps/rust_demo-cac7a90277a49665.rmeta: src/main.rs
+
+/Users/mzc/Documents/project_self/rust-demo/target/debug/deps/rust_demo-cac7a90277a49665.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust_demo-e108f7885a7ed83c.d
@@ -0,0 +1,5 @@
+/Users/mzc/Documents/project_self/rust-demo/target/debug/deps/rust_demo-e108f7885a7ed83c.rmeta: src/main.rs
+
+/Users/mzc/Documents/project_self/rust-demo/target/debug/deps/rust_demo-e108f7885a7ed83c.d: src/main.rs
+
+src/main.rs:
