544eb7dfbf9ca5ca5ffe21616f05fed436cd06b1
赵雅国 <zhao.yaguo@shinewingdigital.com>
2021-12-13 16:52:58 +0800

    fix(merge)

--- target/debug/deps/rust_demo-cac7a90277a49665.d
+++
@@ -1,5 +0,0 @@
-/Users/mzc/Documents/project_self/rust-demo/target/debug/deps/rust_demo-cac7a90277a49665.rmeta: src/main.rs
-
-/Users/mzc/Documents/project_self/rust-demo/target/debug/deps/rust_demo-cac7a90277a49665.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/rust_demo-e108f7885a7ed83c.d
+++
@@ -1,5 +0,0 @@
-/Users/mzc/Documents/project_self/rust-demo/target/debug/deps/rust_demo-e108f7885a7ed83c.rmeta: src/main.rs
-
-/Users/mzc/Documents/project_self/rust-demo/target/debug/deps/rust_demo-e108f7885a7ed83c.d: src/main.rs
-
-src/main.rs:
