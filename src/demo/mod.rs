ca7ce5ca5e606e0c8f567335ffad5a27c272881d
赵雅国 <zhao.yaguo@shinewingdigital.com>
2021-12-29 18:11:34 +0800

    feat(enum)

--- src/demo/mod.rs
+++ src/demo/mod.rs
@@ -5,6 +5,7 @@ pub mod demo4;
 pub mod demo5;
 pub mod demo6;
 pub mod demo7;
+pub mod demo8;
 
 pub use demo1::*;
 pub use demo2::*;
@@ -13,3 +14,4 @@ pub use demo4::*;
 pub use demo5::*;
 pub use demo6::*;
 pub use demo7::*;
+pub use demo8::*;
